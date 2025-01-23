bb801e895a069df48a1774c0d4cbb61ea2180d89
Miłosz <milpie51@gmail.com>
2025-01-23 20:28:28 +0100

    Added SearchBar in Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,31 @@
 import React from 'react';
-import { View, Text, ImageBackground, StyleSheet, Dimensions } from 'react-native';
+import { View, Text, ImageBackground, StyleSheet, Dimensions, TextInput } from 'react-native';
 
 // Pobranie szerokości ekranu
 const { width } = Dimensions.get('window');
 
 const Header = () => {
   return (
-    <View style={{ marginBottom: 10 }}>
+    <View style={styles.container}>
       <ImageBackground
         style={styles.backgroundImage}
         source={require("../assets/images/backgroundImage.jpg")}
       >
+        {/* Overlay dla lepszej czytelności */}
         <View style={styles.overlay} />
 
+        {/* Tekst na obrazie */}
         <View style={styles.textContainer}>
           <Text style={styles.headerText}>Explore Events Near You!</Text>
+        </View>
+
+        {/* Search Bar na dole ImageBackground */}
+        <View style={styles.searchBarContainer}>
+          <TextInput
+            style={styles.searchBar}
+            placeholder="Search for events"
+            placeholderTextColor="#aaa"
+          />
         </View>
       </ImageBackground>
     </View>
@@ -24,16 +35,20 @@ const Header = () => {
 export default Header;
 
 const styles = StyleSheet.create({
-  backgroundImage: {
+  container: {
+    marginBottom: 20,
     width: width, // Pełna szerokość ekranu
-    height: 180, 
-    resizeMode: "cover", // Obraz wypełni przestrzeń, zachowując proporcje
+  },
+  backgroundImage: {
+    width: width, 
+    height: 190, 
+    resizeMode: "cover",
     justifyContent: "center",
     alignItems: "center",
   },
   overlay: {
-    ...StyleSheet.absoluteFillObject, 
-    backgroundColor: 'rgba(0, 0, 0, 0.58)', // Ciemniejszy overlay dla lepszej czytelności
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'rgba(0, 0, 0, 0.58)',
   },
   textContainer: {
     padding: 10,
@@ -44,5 +59,28 @@ const styles = StyleSheet.create({
     fontWeight: "400",
     textAlign: "center",
     letterSpacing: 1,
+  },
+  searchBarContainer: {
+    position: "absolute",
+    bottom: 10, // Wystaje poza obrazek
+    left: "5%",
+    width: "90%", // Szerokość paska wyszukiwania
+    alignItems: "center",
+  },
+  searchBar: {
+    backgroundColor: "transparent", // Delikatne tło dla lepszej czytelności
+    borderColor: "white", // Białe obramowanie
+    borderWidth: 1.5, // Grubość obramowania
+    width: "100%",
+    height: 40,
+    borderRadius: 20,
+    paddingHorizontal: 15,
+    fontSize: 16,
+    color: "white", // Tekst wewnątrz SearchBar również biały
+    elevation: 5, // Cień (Android)
+    shadowColor: "#000", // Cień (iOS)
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
   },
 });
